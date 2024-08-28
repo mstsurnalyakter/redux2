7cb6d994e6418b6fd02cdf827a053645d5af7cd8
mstsurnalyakter <surnalyms@gmail.com>
2024-08-28 13:16:16 +0600

    add index.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const { createStore } = require("redux");
 
-const INCREMENT="INCREMENT";
-const DECREMENT="DECREMENT";
+//Define constant
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+const DECREMENT_BY_VALUE = "DECREMENT_BY_VALUE";
+const ADD_USERS = "ADD_USERS";
 
-const initialState = {
+//state:count-1
+const initialCounter = {
+    users:["Mst Surnaly Akter"],
     count:0
 }
+const initialUser = {
+    users:["Mst Surnaly Akter"],
+    count:1
+}
 
-const incrementCounter = () =>{
+const addUser = (user) =>{
+    return {
+      type: ADD_USERS,
+      payload:user
+
+
+    };
+}
+
+
+//action:Increment,decrement,reset
+const incrementCounter = ()=>{
     return{
         type:INCREMENT
     }
@@ -19,33 +41,90 @@ const decrementCounter = () =>{
     }
 }
 
-const counterReducer = (state=initialState,action)=>{
-    switch (action.type) {
-        case INCREMENT:
-        return{
-            ...state,
-            count:state.count + 1
-        }
-
-        case DECREMENT:
-            return{
-                ...state,
-                count:state.count - 1
-            }
-
-        default:
-            state
+const resetCounter = () =>{
+    return{
+        type:RESET
     }
 }
 
+const incrementCounterByValue = (value) =>{
+    return{
+        type:INCREMENT_BY_VALUE,
+        payload:value
+
+    }
+}
+const decrementCounterByValue = value =>{
+    return{
+        type:DECREMENT_BY_VALUE,
+        payload:value
+    }
+}
+
+
+
+//reducer
+const counterReducer = (state=initialCounter,action)=>{
+    switch (action.type) {
+      case INCREMENT:
+        return {
+          ...state,
+          count: state.count + 1,
+        };
+      case DECREMENT:
+        return {
+          ...state,
+          count: state.count - 1,
+        };
+      case INCREMENT_BY_VALUE:
+        return {
+          ...state,
+          count: state.count + action.payload,
+        };
+
+    case DECREMENT_BY_VALUE:
+        return{
+            ...state,
+            count:state.count - state.payload
+        }
+
+
+      case RESET:
+        return {
+          ...state,
+          count: 0,
+        };
+
+      default:
+        state;
+    }
+}
+
+const userReducer = (state=initialUser,action)=>{
+   switch (action.type) {
+    case ADD_USERS:
+        return{
+            users:[...state.users,action.payload],
+            count:state.count+1
+        }
+
+    default:
+       state
+   }
+}
+
+//store
 const store = createStore(counterReducer);
+const userStore = createStore(userReducer);
+
+userStore.subscribe(()=>{
+    console.log(userStore.getState());
+})
+
 store.subscribe(()=>{
     console.log(store.getState());
 })
 
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(decrementCounter())
-store.dispatch(decrementCounter())
+//action dispatch
+store.dispatch(incrementCounterByValue(5));
+userStore.dispatch(addUser("Salma"))
